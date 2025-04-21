88f26129f3832359de396371ffe2fbfa2b20172b
ErfanAalam <erfankhansiwani@gmail.com>
2025-04-21 14:53:10 +0530

    origin updated for changes in ui

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const server = createServer(app);
 
 app.use(
   cors({
-    origin:  ["https://chatapp-frontend-dq1n.onrender.com"],
+    // origin:  ["https://chatapp-frontend-dq1n.onrender.com"],
+    origin:  ["http://localhost:5173"],
+    // http://localhost:5173
     credentials: true,
   })
 );
@@ -49,7 +51,8 @@ app.get("/", (req, res) => {
 
 const io = new Server(server, {
   cors: {
-    origin:  ["https://chatapp-frontend-dq1n.onrender.com"],
+    // origin:  ["https://chatapp-frontend-dq1n.onrender.com"],
+    origin:  ["http://localhost:5173"],
     methods: ["POST", "GET"],
   },
 });
